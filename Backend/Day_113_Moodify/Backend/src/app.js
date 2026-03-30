a61f4409d4d97de1e4a2dea0b3d845054742d601
PiyushG-git <piyushg251004@gmail.com>
2026-03-30 20:23:57 +0530

    integrated the frontend

--- Backend/Day_113_Moodify/Backend/src/app.js
+++ Backend/Day_113_Moodify/Backend/src/app.js
@@ -1,10 +1,15 @@
 const express=require("express")
 const cookiePraser=require("cookie-parser")
+const cors =require("cors")
 
 const app=express()
 
 app.use(express.json())
 app.use(cookiePraser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 const authRoutes=require("./routes/auth.routes")
 
